0455b07073df5ecb6d0ab5edd0ab55968aa05554
dlacy27 <dlacy@search.org>
2016-07-26 18:57:35 -0600

    Updated AutomaticWeaponIndicator in Baseline to comply with Rule 258.
    The baseline has TypeOfWeaponForceInvolved = None, so
    AutomaticWeaponIndicator needs to be blank.

--- tools/nibrs-validation/src/test/java/org/search/nibrs/validation/BaselineIncidentFactory.java
+++ tools/nibrs-validation/src/test/java/org/search/nibrs/validation/BaselineIncidentFactory.java
@@ -47,7 +47,7 @@ final class BaselineIncidentFactory {
 		o.setBiasMotivation(0, "15");
 		o.setLocationType("15");
 		o.setNumberOfPremisesEntered(null);
-		o.setAutomaticWeaponIndicator(0, "A");
+		o.setAutomaticWeaponIndicator(0, " ");
 		
 		Victim v = new Victim();
 		incident.addVictim(v);
