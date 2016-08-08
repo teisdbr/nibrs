f49784b09c7bcc2bf4457a6a72370ccfea5e69b2
criffle <criffle@search.org>
2016-08-08 16:09:11 -0400

    added a first version of the nibrs validation method

--- tools/nibrs-validation/src/main/java/org/search/nibrs/validation/NIBRSValidator.java
+++ tools/nibrs-validation/src/main/java/org/search/nibrs/validation/NIBRSValidator.java
@@ -1,5 +1,34 @@
+
 package org.search.nibrs.validation;
 
-public class NIBRSValidator {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.search.nibrs.common.NIBRSError;
+import org.search.nibrs.model.NIBRSSubmission;
+
+public class NIBRSValidator {
+	
+	
+	public NIBRSValidator() {
+	
+	}
+	
+	public List<NIBRSError> validate(NIBRSSubmission nibrsSubmission, 
+			ValidationListener validationListener){
+		
+		List<NIBRSError> nibrsErrorList = new ArrayList<NIBRSError>();
+		
+		
+		validationListener.validationAvailable(nibrsErrorList);
+		
+		return nibrsErrorList;
+	}
+
+	
+	abstract class ValidationListener{
+		
+		abstract void validationAvailable(List<NIBRSError> nibrsErrorList);
+	}
+	
 }
