ea88a8a8284e347635a38559426e91cf00012b9e
Scott Came <scott@search.org>
2016-11-22 18:05:26 -0800

    Implemented rule 704 (month and year of tape)

--- tools/nibrs-validation/src/main/java/org/search/nibrs/validation/groupb/GroupBArrestReportRulesFactory.java
+++ tools/nibrs-validation/src/main/java/org/search/nibrs/validation/groupb/GroupBArrestReportRulesFactory.java
@@ -3,9 +3,11 @@ package org.search.nibrs.validation.groupb;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.search.nibrs.common.NIBRSError;
 import org.search.nibrs.model.GroupBArrestReport;
 import org.search.nibrs.model.codes.NIBRSErrorCode;
 import org.search.nibrs.validation.rules.NotBlankRule;
+import org.search.nibrs.validation.rules.NumericValueRule;
 import org.search.nibrs.validation.rules.Rule;
 
 /**
@@ -29,11 +31,49 @@ public class GroupBArrestReportRulesFactory {
 		rulesList.add(getRule101("ori", "1"));
 		rulesList.add(getRule101("yearOfTape", "Year of Tape"));
 		rulesList.add(getRule101("monthOfTape", "Month of Tape"));
+		rulesList.add(getRule104("yearOfTape"));
+		rulesList.add(getRule104("monthOfTape"));
 		return rulesList;
 	}
 	
 	Rule<GroupBArrestReport> getRule101(String propertyName, String dataElementIdentifier) {
 		return new NotBlankRule<GroupBArrestReport>(propertyName, dataElementIdentifier, GroupBArrestReport.class, NIBRSErrorCode._701);
+	}
+	
+	Rule<GroupBArrestReport> getRule104(String propertyName) {
+		Rule<GroupBArrestReport> ret = null;
+		if ("yearOfTape".equals(propertyName)) {
+			ret = new NumericValueRule<>(
+					subject -> {
+						return subject.getYearOfTape();
+					},
+					(value, target) -> {
+						NIBRSError e = null;
+						if (value != null && 1991 > value.intValue()) {
+							e = target.getErrorTemplate();
+							e.setNIBRSErrorCode(NIBRSErrorCode._104);
+							e.setDataElementIdentifier("Year of Tape");
+							e.setValue(value);
+						}
+						return e;
+					});
+		} else if ("monthOfTape".equals(propertyName)) {
+			ret = new NumericValueRule<>(
+					subject -> {
+						return subject.getMonthOfTape();
+					},
+					(value, target) -> {
+						NIBRSError e = null;
+						if (value != null && (1 > value.intValue() || 12 < value.intValue())) {
+							e = target.getErrorTemplate();
+							e.setNIBRSErrorCode(NIBRSErrorCode._104);
+							e.setDataElementIdentifier("Month of Tape");
+							e.setValue(value);
+						}
+						return e;
+					});
+		}
+		return ret;
 	}
 	
 }
