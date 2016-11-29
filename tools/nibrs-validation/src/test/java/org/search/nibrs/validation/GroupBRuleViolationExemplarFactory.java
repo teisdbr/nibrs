f971920c5cdc2e44b804a7d9d73092c105bc5abc
dlacy27 <dlacy@search.org>
2016-11-29 07:01:16 -0700

    Added Rule 701 Data Element 42 Arrest Date

--- tools/nibrs-validation/src/test/java/org/search/nibrs/validation/GroupBRuleViolationExemplarFactory.java
+++ tools/nibrs-validation/src/test/java/org/search/nibrs/validation/GroupBRuleViolationExemplarFactory.java
@@ -73,7 +73,9 @@ final class GroupBRuleViolationExemplarFactory {
 			copy = new GroupBArrestReport(arrestReport);
 			copy.getArrestees().get(0).setArrestTransactionNumber(null);
 			reports.add(copy);
-			
+			copy = new GroupBArrestReport(arrestReport);
+			copy.getArrestees().get(0).setArrestDate(null);
+			reports.add(copy);
 			
 			
 			
