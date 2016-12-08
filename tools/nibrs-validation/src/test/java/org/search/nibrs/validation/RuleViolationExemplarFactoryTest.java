0b2c351fc8fc97f34e2be74c73d6c5f013de3e7c
Scott Came <scott@search.org>
2016-12-08 13:54:11 -0800

    Remove unnecessary dependency of nibrs-validation on nibrs-xml

--- tools/nibrs-validation/src/test/java/org/search/nibrs/validation/RuleViolationExemplarFactoryTest.java
+++ tools/nibrs-validation/src/test/java/org/search/nibrs/validation/RuleViolationExemplarFactoryTest.java
@@ -16,22 +16,15 @@
 package org.search.nibrs.validation;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
-import org.search.nibrs.common.NIBRSError;
+import org.search.nibrs.model.AbstractReport;
 import org.search.nibrs.model.GroupAIncidentReport;
 import org.search.nibrs.model.NIBRSSubmission;
-import org.search.nibrs.model.AbstractReport;
-import org.search.nibrs.xml.XmlUtils;
-import org.search.nibrs.xml.exporter.XMLExporter;
-import org.w3c.dom.Document;
 
 public class RuleViolationExemplarFactoryTest {
 	
@@ -45,18 +38,6 @@ public class RuleViolationExemplarFactoryTest {
 		RuleViolationExemplarFactory exemplarFactory = RuleViolationExemplarFactory.getInstance();
 		AbstractReport incident = exemplarFactory.getGroupAIncidentsThatViolateRule(115).get(0);
 		report.addReport(incident);
-		Document d = new XMLExporter().convertNIBRSSubmissionToDocument(report, new ArrayList<NIBRSError>());
-		//XmlUtils.printNode(d, System.out);
-		String incidentNumberValue = XmlUtils.xPathStringSearch(d, "/nibrs:Submission/nibrs:AbstractReport/nc:Incident/nc:ActivityIdentification/nc:IdentificationID");
-		assertTrue(incidentNumberValue.trim().contains(" "));
-		List<AbstractReport> incidents = new ArrayList<AbstractReport>();
-		incidents.addAll(exemplarFactory.getGroupAIncidentsThatViolateRule(119));
-		assertEquals(1, incidents.size());
-		report = new NIBRSSubmission();
-		report.addReports(incidents);
-		d = new XMLExporter().convertNIBRSSubmissionToDocument(report, new ArrayList<NIBRSError>());
-		//XmlUtils.printNode(d, System.out);
-		assertNotNull(XmlUtils.xPathNodeSearch(d, "/nibrs:Submission/nibrs:AbstractReport/j:OffenseSegment[nibrs:OffenseUCRCode='120']"));
 	}
 	
 	@Test
