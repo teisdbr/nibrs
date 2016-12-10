71b74b8539a00e3ead720cb768c410dca038ebb1
Scott Came <scott@search.org>
2016-12-10 05:40:04 -0800

    Fix copyright notice

--- tools/nibrs-apps/src/main/java/org/search/nibrs/apps/NIBRSValidator.java
+++ tools/nibrs-apps/src/main/java/org/search/nibrs/apps/NIBRSValidator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2016 SEARCH-The National Consortium for Justice Information and Statistics
+ * Copyright 2016 Research Triangle Institute
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
