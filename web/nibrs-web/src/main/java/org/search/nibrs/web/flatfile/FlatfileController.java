1e3f4e4f2600443b6d1c416d1c4cab0b22ccaf6e
Haiqi Wei <haiqi@search.org>
2017-04-05 15:40:25 -0500

    Disable spring security and add a very basic file submission form.

--- web/nibrs-web/src/main/java/org/search/nibrs/web/flatfile/FlatfileController.java
+++ web/nibrs-web/src/main/java/org/search/nibrs/web/flatfile/FlatfileController.java
@@ -1,8 +1,6 @@
 package org.search.nibrs.web.flatfile;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -16,25 +14,28 @@ import org.search.nibrs.model.AbstractReport;
 import org.search.nibrs.validation.SubmissionValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.ResourceUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class FlatfileController {
-	@RequestMapping("/")
-    public String index(Model model) throws IOException {
-		Reader inputReader = null;
+	@GetMapping("/")
+	public String listUploadedFiles(Model model) throws IOException {
+	
+	    return "submissionForm";
+	}
+	
+    @PostMapping("/")
+	public String handleFileUpload(@RequestParam("file") MultipartFile multipartFile,
+			RedirectAttributes redirectAttributes, Model model) throws IOException {
+
 		String readerLocationName = "console";
 		
-		File file;
-		try {
-			file = ResourceUtils.getFile("src/test/resources/test.txt");
-			inputReader = new BufferedReader(new
-					InputStreamReader(getClass().getClassLoader().getResourceAsStream("instances/test.txt")));
-			readerLocationName = file.getAbsolutePath();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+		Reader inputReader = new BufferedReader(new
+				InputStreamReader(multipartFile.getInputStream()));
 
 		IncidentBuilder incidentBuilder = new IncidentBuilder();
 		SubmissionValidator submissionValidator = new SubmissionValidator();
@@ -52,6 +53,9 @@ public class FlatfileController {
 		incidentBuilder.buildIncidents(inputReader, readerLocationName);
 
 		model.addAttribute("errorList", errorList);
-        return "flatfileUpload";
+        return "validationReport";
     }
+
+	
 }
+
