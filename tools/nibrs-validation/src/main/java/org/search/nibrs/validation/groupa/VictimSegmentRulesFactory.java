7bc1d7a01a8de7ed41937856e2d8907594ddd330
Haiqi Wei <haiqi@search.org>
2017-04-27 10:58:16 -0500

    Fix rule 458 and 459 to skip the related offender number 00.

--- tools/nibrs-validation/src/main/java/org/search/nibrs/validation/groupa/VictimSegmentRulesFactory.java
+++ tools/nibrs-validation/src/main/java/org/search/nibrs/validation/groupa/VictimSegmentRulesFactory.java
@@ -754,7 +754,7 @@ public class VictimSegmentRulesFactory {
 
 				List<Integer> offenderList = new ArrayList<>();
 				offenderList.addAll(victimSegment.getDistinctValidRelatedOffenderNumberList());
-				offenderList.removeIf(item -> item == null);
+				offenderList.removeIf(item -> (item == null || item == 0));
 
 				if (!victimSegment.isPerson() && !offenderList.isEmpty()) {
 					e = victimSegment.getErrorTemplate();
@@ -778,7 +778,7 @@ public class VictimSegmentRulesFactory {
 
 				List<Integer> offenderNumberList = new ArrayList<>();
 				offenderNumberList.addAll(victimSegment.getDistinctValidRelatedOffenderNumberList());
-				offenderNumberList.removeIf(item -> item == null);
+				offenderNumberList.removeIf(item -> item == null || item == 0);
 
 				List<String> offenseCodeList = new ArrayList<>();
 				offenseCodeList.addAll(victimSegment.getUcrOffenseCodeList());
