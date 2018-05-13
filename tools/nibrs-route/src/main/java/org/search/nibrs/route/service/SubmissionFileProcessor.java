96595549d038af271fbc2de412ea92671b1565f2
Haiqi Wei <haiqi@search.org>
2018-05-12 21:34:42 -0500

    Add the ability of valiating XML submissions.

--- tools/nibrs-route/src/main/java/org/search/nibrs/route/service/SubmissionFileProcessor.java
+++ tools/nibrs-route/src/main/java/org/search/nibrs/route/service/SubmissionFileProcessor.java
@@ -18,28 +18,36 @@ package org.search.nibrs.route.service;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.Writer;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.apache.camel.Body;
 import org.apache.camel.Header;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.tika.exception.TikaException;
 import org.search.nibrs.common.NIBRSError;
 import org.search.nibrs.flatfile.errorexport.ErrorExporter;
 import org.search.nibrs.flatfile.importer.IncidentBuilder;
 import org.search.nibrs.importer.ReportListener;
 import org.search.nibrs.model.AbstractReport;
+import org.search.nibrs.util.NibrsFileUtils;
 import org.search.nibrs.validation.SubmissionValidator;
+import org.search.nibrs.xmlfile.importer.XmlIncidentBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.xml.sax.SAXException;
 
 @Component
 @Scope("prototype")
@@ -49,30 +57,27 @@ public class SubmissionFileProcessor {
 	@Autowired
 	IncidentBuilder incidentBuilder;
 	@Autowired
+	XmlIncidentBuilder xmlIncidentBuilder;
+	@Autowired
 	SubmissionValidator submissionValidator;
 	@Autowired
 	ErrorExporter errorExporter;
 	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
 		
-	public ValidationResults validate(@Body File file) throws IOException{
+	public ValidationResults validate(@Body File file) throws IOException, ParserConfigurationException, TikaException, SAXException{
 		ValidationResults validationResults = new ValidationResults();
 		
-		Reader inputReader = new BufferedReader(new FileReader(file));
-		String readerLocationName = file.getAbsolutePath();
-
-		incidentBuilder.addIncidentListener(new ReportListener() {
+		ReportListener validatorListener = new ReportListener() {
 			@Override
 			public void newReport(AbstractReport report, List<NIBRSError> el) {
 				validationResults.getErrorList().addAll(el);
 				validationResults.getErrorList().addAll(submissionValidator.validateReport(report));
 				addReportWithoutErrors(validationResults, report);
 			}
+		};
 
-		});
+		validateFile(validatorListener, file);
 
-		incidentBuilder.buildIncidents(inputReader, readerLocationName);
-
-		inputReader.close();
 		return validationResults; 
 
 	}
@@ -98,7 +103,9 @@ public class SubmissionFileProcessor {
 		File resultDirectory = new File(resultPath);
 		resultDirectory.mkdir();
 		
-		String fileName = resultPath + File.separator + fileNameOnly.replace(".", "-" + formatter.format(LocalDateTime.now()) + ".");
+		String baseName = FilenameUtils.getBaseName(fileNameOnly);
+		
+		String fileName = resultPath + File.separator + baseName + "-" + formatter.format(LocalDateTime.now()) + ".txt";
 		File file = new File(fileName);
 		FileWriter filewriter = new FileWriter(file);
 		Writer outputWriter = new BufferedWriter(filewriter);
@@ -107,6 +114,33 @@ public class SubmissionFileProcessor {
 		log.info("The error report is writen to " + fileName); 
 		
 		return file; 
+	}
+	
+	//TODO create another common project to contain the validateFile() and validateInputStream() methods. 
+	private static void validateFile(ReportListener validatorlistener, 
+			File file) throws ParserConfigurationException, IOException, TikaException, SAXException {
+		String fileType = NibrsFileUtils.getMediaType(file);
+		FileInputStream inputStream = new FileInputStream(file);
+
+		switch (fileType){
+		case "text/xml":
+		case "application/xml":
+			XmlIncidentBuilder xmlIncidentBuilder = new XmlIncidentBuilder();
+			xmlIncidentBuilder.addIncidentListener(validatorlistener);
+			xmlIncidentBuilder.buildIncidents(inputStream, file.getAbsolutePath());
+			break; 
+		case "text/plain": 
+		case "application/octet-stream": 
+			Reader inputReader = new BufferedReader(new InputStreamReader(inputStream));
+			IncidentBuilder incidentBuilder = new IncidentBuilder();
+			incidentBuilder.addIncidentListener(validatorlistener);
+			incidentBuilder.buildIncidents(inputReader, file.getAbsolutePath());
+			inputReader.close();
+			break;
+		default:
+			System.err.println("The file type " + fileType + " is not supported"); 
+		}
+		
 	}
 	
 }
