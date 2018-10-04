58a0ae162d1a2d3ea548bb9e6e2941a1a4d204e3
Scott Came <scottcame10@gmail.com>
2018-10-04 09:43:38 -0700

    Switch from openxlsx to readxl

--- tools/r-packages/nibrs/R/LoadCodeTables.R
+++ tools/r-packages/nibrs/R/LoadCodeTables.R
@@ -13,17 +13,17 @@
 # limitations under the License.
 
 #' @importFrom DBI dbClearResult dbSendQuery dbWriteTable
-#' @importFrom openxlsx read.xlsx
+#' @importFrom readxl read_excel
 #' @import dplyr
 #' @import purrr
 #' @import tibble
 #' @export
 loadCodeTables <- function(spreadsheetFile, conn) {
 
-  TOC <- read.xlsx(spreadsheetFile, sheet='TOC')
+  TOC <- read_excel(spreadsheetFile, sheet='TOC')
 
   map2(TOC$Table, TOC$Tab, function(codeTableName, tabName) {
-    ct <- read.xlsx(spreadsheetFile, sheet=tabName) %>%
+    ct <- read_excel(spreadsheetFile, sheet=tabName) %>%
       select(-starts_with('X_')) %>% as_tibble()
     writeLines(paste0("Loading code table: ", codeTableName))
     dbClearResult(dbSendQuery(conn, paste0("truncate ", codeTableName)))
@@ -38,7 +38,12 @@ loadCodeTables <- function(spreadsheetFile, conn) {
       colnames(ct) <- c(paste0(codeTableName, 'ID'), 'StateCode', 'StateDescription')
     }
     ct <- ct %>% mutate(NIBRSCode=StateCode, NIBRSDescription=StateDescription) %>%
-      mutate_if(is.double, as.integer)
+      mutate_at(vars(ends_with('ID')), as.integer) %>%
+      mutate_at(vars(-ends_with('ID')), as.character) %>%
+      mutate(
+        StateCode=case_when(is.na(StateCode) & StateDescription=='Blank' ~ ' ', TRUE ~ StateCode),
+        NIBRSCode=case_when(is.na(NIBRSCode) & NIBRSDescription=='Blank' ~ ' ', TRUE ~ NIBRSCode)
+      )
     dbWriteTable(conn=conn, value=ct, name=codeTableName, append=TRUE, row.names = FALSE)
     attr(ct, 'type') <- 'CT'
     ct
